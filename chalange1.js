835c89b18e8a568d0e5b1dd593e511f23b9fb96b
ABHINAND <abhinandazokdio@gmail.com>
2021-10-21 14:31:33 +0530

    js chalange 1

---
+++ chalange1.js
@@ -0,0 +1,15 @@
+var a='14'  ,b=15;
+
+if (typeof a== 'string'){
+    console.log('A is a String')
+}
+else{
+    console.log('A is a integer')
+}
+
+if (typeof b== 'string'){
+    console.log('b is a String')
+}
+else{
+    console.log('b is a integer')
+}
